a3146afa2b4385f58af7eb4ab882f0d77a659f38
JEMata16 <josellique@gmail.com>
2024-11-25 20:04:47 -0600

    Try with cookie

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ client.on("messageCreate", async (message) => {
         requestOptions: {
           headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            'cookie': process.env.COOKIE
           },
         },
       });
