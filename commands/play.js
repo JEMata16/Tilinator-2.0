7db792d0739e1758a0897f4e9e5ee86df35c6d1d
JEMata16 <josellique@gmail.com>
2024-11-26 20:12:53 -0600

    Test on play

--- commands/play.js
+++ commands/play.js
@@ -12,49 +12,48 @@ module.exports = {
 			.setRequired(true)
 		),
 	async execute(interaction) {
-        interaction.deferReply();
-		const url = interaction.options.getString("url");
-		// Command logic here
-		if (!ytdl.validateURL(url)) {
-			return interaction.reply({ content: 'Nononono Url invalid', ephemeral: true });
-		}
+        interaction.reply("TEST DE CHOROCONTGA MUSICA");
+		// const url = interaction.options.getString("url");
+		// if (!ytdl.validateURL(url)) {
+		// 	return interaction.reply({ content: 'Nononono Url invalid', ephemeral: true });
+		// }
 
-		const channel = interaction.member?.voice.channel;
-		if (!channel) {
-			return interaction.reply({ content: "Where is your voice channel? Dude", ephemeral: true });
-		}
+		// const channel = interaction.member?.voice.channel;
+		// if (!channel) {
+		// 	return interaction.reply({ content: "Where is your voice channel? Dude", ephemeral: true });
+		// }
 
-		try {
-			const connection = joinVoiceChannel({
-				channelId: channel.id,
-				guildId: interaction.guild.id,
-				adapterCreator: interaction.guild.voiceAdapterCreator,
-			});
-			const player = createAudioPlayer();
-            const stream = await ytdl(url, {
-                filter: 'audioonly',
-                quality: 'highestaudio',
-                requestOptions: {
-                  headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-                  },
-                },
-              });
+		// try {
+		// 	const connection = joinVoiceChannel({
+		// 		channelId: channel.id,
+		// 		guildId: interaction.guild.id,
+		// 		adapterCreator: interaction.guild.voiceAdapterCreator,
+		// 	});
+		// 	const player = createAudioPlayer();
+        //     const stream = await ytdl(url, {
+        //         filter: 'audioonly',
+        //         quality: 'highestaudio',
+        //         requestOptions: {
+        //           headers: {
+        //             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        //           },
+        //         },
+        //       });
 
-            stream.on("info", () => console.log("Stream obtenido con éxito"));
-            stream.on("error", (err) => console.error("Error en el stream:", err));
-			const resource = createAudioResource(stream);
-			connection.subscribe(player);
-			player.play(resource);
-            player.on(AudioPlayerStatus.Idle, () => {
-                console.log("Reproducción finalizada.");
-                connection.destroy();
-            });
+        //     stream.on("info", () => console.log("Stream obtenido con éxito"));
+        //     stream.on("error", (err) => console.error("Error en el stream:", err));
+		// 	const resource = createAudioResource(stream);
+		// 	connection.subscribe(player);
+		// 	player.play(resource);
+        //     player.on(AudioPlayerStatus.Idle, () => {
+        //         console.log("Reproducción finalizada.");
+        //         connection.destroy();
+        //     });
 
-			await interaction.reply(`Playing: ${url}`);
-		} catch (error) {
-			console.error(error);
-			await interaction.reply({ content: 'Error playing music!', ephemeral: true });
-		}
+		// 	await interaction.reply(`Playing: ${url}`);
+		// } catch (error) {
+		// 	console.error(error);
+		// 	await interaction.reply({ content: 'Error playing music!', ephemeral: true });
+		// }
 	},
 };
